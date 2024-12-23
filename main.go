836312b1349a95bbd3e919261be11cf55caceee8
Heinz Knutzen <heinz.knutzen@gmail.com>
2024-12-23 20:44:59 +0100

    Details are updated successfully

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"embed"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+)
+
+var (
+	//go:embed all:templates/*
+	templateFS embed.FS
+
+	// Parsed templates
+	html *template.Template
+)
+
+func main() {
+	var err error
+	htmlFS, _ := fs.Sub(templateFS, "templates")
+	html, err = template.ParseFS(htmlFS, "*.html")
+	if err != nil {
+		panic(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("GET /", http.HandlerFunc(index))
+	mux.Handle("GET /details/{id}", http.HandlerFunc(updateDetails))
+	log.Fatal(http.ListenAndServe(":8080", handlers.RecoveryHandler()(mux)))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	table := []int{1, 2, 3, 4}
+	arg := map[string]any{"table": table, "details": table[0]}
+	if err := html.ExecuteTemplate(w, "index.html", arg); err != nil {
+		panic(err)
+	}
+}
+
+func updateDetails(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if err := html.ExecuteTemplate(w, "details.html", id); err != nil {
+		panic(err)
+	}
+}
