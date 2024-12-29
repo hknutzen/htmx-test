cf5f834477a6f52bbf4bc6d621371bc7c5eb9e66
Heinz Knutzen <heinz.knutzen@gmail.com>
2024-12-29 23:43:52 +0100

    Mit bootstrap v5.3.3, 19999 Zeilen

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
 	"io/fs"
 	"log"
@@ -16,6 +17,9 @@ var (
 
 	//go:embed htmx.min.js
 	htmx embed.FS
+
+	//go:embed bootstrap.min.css
+	bootstrap embed.FS
 
 	// Parsed templates
 	html *template.Template
@@ -32,12 +36,16 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("GET /", http.HandlerFunc(index))
 	mux.Handle("GET /htmx.min.js", http.FileServer(http.FS(htmx)))
+	mux.Handle("GET /bootstrap.min.css", http.FileServer(http.FS(bootstrap)))
 	mux.Handle("GET /details/{id}", http.HandlerFunc(updateDetails))
 	log.Fatal(http.ListenAndServe(":8080", handlers.RecoveryHandler()(mux)))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	table := []int{1, 2, 3, 4}
+	table := []string{}
+	for i := range 19999 {
+		table = append(table, fmt.Sprintf("Service-%d", i+1))
+	}
 	arg := map[string]any{"table": table, "details": table[0]}
 	if err := html.ExecuteTemplate(w, "index.html", arg); err != nil {
 		panic(err)
